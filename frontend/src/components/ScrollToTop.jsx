7cb970b2c85f9911b64e69c26572ab56f2d1e85d
Christian R. Gomelan <100290998+chrisgomelan@users.noreply.github.com>
2026-03-14 07:44:05 +0800

    feat: Implement a ScrollToTop component to provide a button for smooth page navigation to the top.

--- frontend/src/components/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop.jsx
@@ -29,7 +29,7 @@ export default function ScrollToTop() {
     }, []);
 
     return (
-        <div className="fixed bottom-28 right-5 z-[999]">
+        <div className="fixed bottom-[4.5rem] right-[4.5rem] z-[999]">
             {isVisible && (
                 <button
                     onClick={scrollToTop}
